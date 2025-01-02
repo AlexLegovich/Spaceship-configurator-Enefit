b01809e33650a55a9a3735f07bc6d1bf54021cbc
Aleksandr GUssev <gussev.sanya@hotmail.com>
2025-01-02 14:31:21 +0200

    changed height of power input

--- src/data/floating.js
+++ src/data/floating.js
@@ -1,21 +1,17 @@
-
 function isInViewport(element) {
-  const rect = element.getBoundingClientRect();
-  return (
-    rect.bottom > 0 && 
-    rect.top < window.innerHeight
-  );
+  const rect = element.getBoundingClientRect()
+  return rect.bottom > 0 && rect.top < window.innerHeight
 }
 
 function handleFloating() {
-  const sectionSummary = document.querySelector('.summary');
-const sectionFloating = document.querySelector('.summary__floating');
+  const sectionSummary = document.querySelector('.summary')
+  const sectionFloating = document.querySelector('.summary__floating')
 
   if (isInViewport(sectionSummary)) {
     sectionFloating.classList.add('hidden')
   } else {
-    sectionFloating.classList.remove('hidden');
+    sectionFloating.classList.remove('hidden')
   }
 }
 
-export{handleFloating}
+export { handleFloating }
